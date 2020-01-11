5367228396c71b3967c95766f4d09a4c66437e4c
DagmawiYohannes3914 <dagmawiyohannes323@gmail.com>
2020-01-11 18:54:33 +0300

    update entity

--- entity/entities.go
+++ entity/entities.go
@@ -26,3 +26,17 @@ type Comment struct {
 	Email    string
 	PlacedAt time.Time
 }
+type Product struct {
+	ID          uint
+	Name        string
+	Ptype       string
+	Price       float64
+	Description string
+	CreatedAt   time.Time
+	//Categories  Category
+	Image string
+}
+type Balance struct {
+	ID          uint
+	YourBalance float64
+}
