889da6039d28103516478affe03b2de7f8610fec
DagmawiYohannes3914 <dagmawiyohannes323@gmail.com>
2020-01-26 10:20:39 +0300

    entity update

--- entity/entities.go
+++ entity/entities.go
@@ -3,14 +3,6 @@ package entity
 import "time"
 
 // Category represents ordermenu Category
-type Category struct {
-	ID          uint
-	Name        string
-	Description string
-	Image       string
-	//Items       []product
-}
-
 // Item represents type of products
 type Product struct {
 	ID          uint
